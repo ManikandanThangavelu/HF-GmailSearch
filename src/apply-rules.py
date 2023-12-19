9e1358063b7310d02c6965bf2d27c96d944d54b9
Manikandan <tmanikandan05@gmail.com>
2023-12-19 22:46:37 +0530

    Updated apply rules to use API instead of DB update

--- src/apply-rules.py
+++ src/apply-rules.py
@@ -2,9 +2,46 @@ import os
 import json
 import sqlite3
 
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 config_dir = os.path.abspath("config")
 conn = sqlite3.connect(f"{config_dir}/emails.db")
+# If modifying these scopes, delete the file token.json
+SCOPES = ["https://mail.google.com/"]
 
+
+def get_google_creds():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(f"{config_dir}/token.json"):
+        creds = Credentials.from_authorized_user_file(f"{config_dir}/token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                f"{config_dir}/client_secret.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open(f"{config_dir}/token.json", "w") as token:
+                token.write(creds.to_json())
+    return creds
+
+
+def get_gmail_service():
+    creds = get_google_creds()
+    service = build("gmail", "v1", credentials=creds)
+    return service
+
+service = get_gmail_service()
 
 def fetch_rules():
     with open(f"{config_dir}/rules.json", 'r') as f:
@@ -12,21 +49,9 @@ def fetch_rules():
     return data
 
 
-def generate_sql_query(root_predicate, rules, actions):
+def generate_sql_query(root_predicate, rules):
     # Define the base SQL query
-    base_query = "UPDATE emails SET "
-
-    # Generate the SET clause based on the actions
-    set_operations = []
-    for action in actions:
-        if action["action"] == 'Mark as':
-            status = action["field"]
-            set_operations.append(f"status = '{status}'")
-        elif action["action"] == 'Move Message':
-            mailbox_type = action["field"]
-            set_operations.append(f"mailbox = '{mailbox_type}'")
-
-    set_clause = ", ".join(set_operations)
+    base_query = "SELECT * FROM emails"
 
     # Generate the WHERE clause based on the rules
     conditions = []
@@ -61,22 +86,38 @@ def generate_sql_query(root_predicate, rules, actions):
     else:
         raise ValueError("Invalid root_predicate. Use 'ALL' or 'ANY'.")
     # Concatenate the base query, SET clause, and WHERE clause
-    full_query = f"{base_query} {set_clause} WHERE {where_clause}"
+    full_query = f"{base_query} WHERE {where_clause}"
     return full_query
+
+
+def apply_actions(emails, actions):
+    email_ids = [email[0] for email in emails]
+    for action in actions:
+        if(action["action"] == 'Mark as' and action["field"] == "READ"):
+            # Mark the email as read
+            service.users().messages().batchModify( userId='me', body={'ids': email_ids, 'removeLabelIds': ['UNREAD']}).execute()
+        elif(action["action"] == 'Mark as' and action["field"] == "UNREAD"):
+            # Mark the email as unread
+            service.users().messages().batchModify( userId='me', body={'ids': email_ids, 'addLabelIds': ['UNREAD']}).execute()
+        elif(action["action"] == 'Move Message'):
+            # Move the email to the 'IMPORTANT' mailbox
+            service.users().messages().batchModify( userId='me', body={'ids': email_ids, 'addLabelIds': [action["field"]]}).execute()
 
 
 def run_query(sql_query):
     cursor = conn.cursor()
     cursor.execute(sql_query)
-    conn.commit()
+    emails = cursor.fetchall()
+    return emails
 
 
 def apply_rules(rules):
     for rule in rules:
         print(f"Applying rule - {rule['id']}")
-        sql_query = generate_sql_query(rule['root_predicate'], rule["rules"], rule["actions"])
+        sql_query = generate_sql_query(rule['root_predicate'], rule["rules"])
         print(f"Query - {sql_query}")
-        run_query(sql_query)
+        emails = run_query(sql_query)
+        apply_actions(emails, rule["actions"])
 
 
 def main():
